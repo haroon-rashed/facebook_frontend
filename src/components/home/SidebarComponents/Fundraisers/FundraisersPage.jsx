51c09c7951ca9c1c824c6eff72b452c428652686
Haroon Rasheed <haronworkspace@gmail.com>
2025-10-31 20:52:39 +0500

    facebook frontend

--- src/components/home/SidebarComponents/Fundraisers/FundraisersPage.jsx
+++ src/components/home/SidebarComponents/Fundraisers/FundraisersPage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Fundraisers from "./Fundraisers";
-import FundRaiserSidebar from "./FundRaiserSidebar";
+// import FundRaiserSidebar from "./FundRaiserSidebar";
 
 const FundraisersPage = () => {
   return (
     <div className="grid grid-cols-12 h-screen overflow-hidden">
       {/* Sidebar */}
       <div className="col-span-3 hide-scrollbar xl:block hidden h-full overflow-y-auto p-4 border-r">
-        <FundRaiserSidebar />
+        {/* <FundRaiserSidebar /> */}
       </div>
 
       {/* Main Content */}
